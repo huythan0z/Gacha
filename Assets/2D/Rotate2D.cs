acb58f6596dd17e3c6da578efb595abaf0b24baa
Taka <huythan0z@gmail.com>
2022-11-01 17:43:51 +0700

    End2D Gacha

---
+++ Assets/2D/Rotate2D.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Xml.Schema;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Rotate2D : MonoBehaviour
+{
+    float speed = 189.243f;
+    bool rotate = false;
+    [SerializeField] Button btnSpin;
+    public Text txtResult;
+    int degreeRandom;
+    int round = 0;
+
+    void Start()
+    {
+        degreeRandom = Random.Range(0, 359);
+        Debug.Log(degreeRandom);
+        btnSpin.onClick.AddListener(spin);
+    }
+    public void spin()
+    {
+        rotate = true;
+    }
+    void Update()
+    {
+
+    }
+    public void FixedUpdate()
+    {
+        int degree = Convert.ToInt32(transform.eulerAngles.z);
+        if (rotate == true)
+        {
+            transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+            if (degree >= degreeRandom && degree <= degreeRandom + 5)
+            {
+                round = round + 1;
+            }
+            if (round > 3)
+            {
+                speed -= 1.0f; 
+            }
+            if (speed <= 0)
+            {
+                speed = 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    if (30 * i <= degree && degree < 30 + 30 * i)
+                    {
+                        txtResult.text = value()[i];
+                    }
+                }
+                btnSpin.onClick.AddListener(spinAgain2D);
+            }
+        }
+    }
+    public void spinAgain2D()
+    {
+        SceneManager.LoadScene("2D");
+    }
+    string[] value()
+    {
+        string[] gift = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+        return gift;
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,85 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using TreeEditor;
+using UnityEditor.SearchService;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using static UnityEngine.GraphicsBuffer;
+using Random = UnityEngine.Random;
+
+public class Rotate : MonoBehaviour
+{
+    float speed = 189.243f;
+    bool spin = false;
+    public GameObject result;
+    public Text txtResult;
+    int degreeRandom;
+    int round = 0;
+    public void Awake()
+    {
+    }
+    public void Start()
+    {
+        degreeRandom = Random.Range(0, 360);
+        Debug.Log(degreeRandom);
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "Spin")
+                {
+                    spin = true;
+                }
+            }
+        }
+        if (spin == true)
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            int degree = Convert.ToInt32(transform.eulerAngles.y);
+            if (degree >= degreeRandom && degree <= degreeRandom + 5)
+            {
+                round = round + 1;
+            }
+            if (round > 3)
+            {
+                speed -= 1.0f;
+            }
+            if (speed <= 0)
+            {
+                speed = 0;
+                for (int i = 0; i < 12; i++)
+                {
+                    if (30 * i < degree && degree < 30 + 30*i)
+                    {
+                        txtResult.text = value()[i];
+                    }
+                }
+                result.SetActive(true);
+            }
+        }
+    }
+    void Update()
+    {
+
+    }
+    public void spinAgain()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    string[] value()
+    {
+        string[] gift = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" };
+        return gift;
+    }
+}
